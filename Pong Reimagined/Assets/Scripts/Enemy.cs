1ed3c64eabb5a48f141827e630a38d21e1ab419f
LoganO3 <92324728+LoganO3@users.noreply.github.com>
2022-01-24 09:25:40 -0800

    Added that basics of the enemy but is needs a lot of work before its usable.

--- Pong Reimagined/Assets/Scripts/Enemy.cs
+++ Pong Reimagined/Assets/Scripts/Enemy.cs
@@ -6,9 +6,8 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float yMin = -8f;
     [SerializeField] float yMax = 8f;
-    [SerializeField] float xMin = 1.3f;
+    [SerializeField] float xMin = 1.5f;
     [SerializeField] float xMax = 18.5f;
-    [SerializeField] float moveSpeed = 2f;
     [SerializeField] float xVelocity = 10f;
     [SerializeField] float yVelocity = 10f;
     Rigidbody2D rB;
@@ -17,26 +16,19 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        puck = FindObjectOfType<Puck>();
         rB = GetComponent<Rigidbody2D>();
+        puck = FindObjectOfType<Puck>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-    }
-
-    private void Move()
-    {
-        if (!puck) { return; }
-        else
-        {
-            var targetPosition = puck.CurrentLocation(); 
-            var movementThisFrame = moveSpeed * Time.deltaTime;
-            targetPosition.y = Mathf.Clamp(targetPosition.y, yMin, yMax);
-            targetPosition.x = Mathf.Clamp(targetPosition.x, xMin, xMax);
-            transform.position = Vector3.MoveTowards (transform.position, targetPosition, movementThisFrame);
-        }
+        rB.velocity = new Vector3(xVelocity, yVelocity, 0);
+        Vector3 mousePostion = Camera.main.ScreenToWorldPoint(puck.CurrentLocation());
+        Vector3 paddlePos = new Vector3(transform.position.x, transform.position.y, 0);
+        paddlePos.y = Mathf.Clamp(mousePostion.y, yMin, yMax);
+        paddlePos.x = Mathf.Clamp(mousePostion.x, xMin, xMax);
+        transform.position = paddlePos;
     }
 }
+
