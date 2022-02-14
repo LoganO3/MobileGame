19061cc9f8934def2b433c7454cd861e0a4c467c
LoganO3 <92324728+LoganO3@users.noreply.github.com>
2022-02-14 09:16:21 -0800

    Made some more minor Changes

--- Pong Reimagined/Assets/Scripts/Timer.cs
+++ Pong Reimagined/Assets/Scripts/Timer.cs
@@ -20,6 +20,49 @@ public class Timer : MonoBehaviour
         string Seconds = ((int)time % 60).ToString();
         string Minutes = ((int)time / 60).ToString();
         string Hours = ((int)time / 3600).ToString();
-        timerText.text = Hours + ":" + Minutes + ":" + Seconds;
+        if (Seconds == "9")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "8")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "7")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "6")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "5")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "4")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "3")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "2")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "1")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else if (Seconds == "0")
+        {
+            timerText.text = Hours + ":" + Minutes + ":0" + Seconds;
+        }
+        else
+        {
+            timerText.text = Hours + ":" + Minutes + ":" + Seconds;
+        }
     }
 }
